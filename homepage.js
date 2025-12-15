5cb01a2c2369273549508bac3c68c9bb6a63f62d
gertaborovci <gb72745@ubt-uni.net>
2025-12-15 16:42:49 +0100

    + top offers

--- homepage.js
+++ homepage.js
@@ -2,3 +2,12 @@ document.querySelector(".search-btn").onclick = () => {
     alert("Searching flights...");
 };
 
+function showMessage() {
+  alert("This feature will be available soon!");
+}
+
+function openJetForm() {
+  alert("Private jet request received. Our team will contact you shortly.");
+}
+
+
